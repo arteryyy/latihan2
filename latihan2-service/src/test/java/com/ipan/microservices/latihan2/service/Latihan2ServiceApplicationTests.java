a4fc8d5c54bd50c6026bad8d5aec1602ed4edcce
IrvanYuatama <113502442+arteryyy@users.noreply.github.com>
2023-01-04 19:46:00 +0700

    Add files via upload

---
+++ latihan2-service/src/test/java/com/ipan/microservices/latihan2/service/Latihan2ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipan.microservices.latihan2.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Latihan2ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
